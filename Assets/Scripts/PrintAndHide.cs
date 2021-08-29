17e1de726e1c58eb4710a68f5b26ddc85d6c7528
taojiajian009 <Airzzz_java@users.noreply.github.com>
2021-08-30 00:27:08 +0800

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,10 +6,13 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i;
     public Renderer rend;
+    private GameObject[] goS;
+    private int a;
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        a = Random.Range(200,251);
     }
 
     // Update is called once per frame
@@ -17,5 +20,21 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         PrintAndHide.print(gameObject.name+":"+i);
+        goS = GameObject.FindGameObjectsWithTag("Red");
+        if(i==100)
+        {
+            foreach(GameObject redGo in goS)
+            {
+                redGo.SetActive(false);
+            }
+        }
+        goS = GameObject.FindGameObjectsWithTag("Blue");
+        if (i ==a)
+        {
+            foreach(GameObject blueGo in goS)
+            {
+                blueGo.GetComponent<Renderer>().enabled=false;
+            }
+        }
     }
 }
